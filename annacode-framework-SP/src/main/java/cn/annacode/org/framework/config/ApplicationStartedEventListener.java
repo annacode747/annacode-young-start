b7a90664cd7c92b2adc2c5be218164cbbbfe7bcf
dj <845632255@qq.com>
2022-11-29 00:31:22 +0800

    配置日志管理

---
+++ annacode-framework-SP/src/main/java/cn/annacode/org/framework/config/ApplicationStartedEventListener.java
@@ -0,0 +1,82 @@
+package cn.annacode.org.framework.config;
+
+import org.slf4j.MDC;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.context.event.ApplicationFailedEvent;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.GenericApplicationListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertySource;
+
+/**
+ * 用于启动监听
+ */
+public class ApplicationStartedEventListener implements GenericApplicationListener {
+
+
+    public static final int DEFAULT_ORDER = Ordered.HIGHEST_PRECEDENCE + 10;
+
+    private static Class<?>[] EVENT_TYPES = { ApplicationStartingEvent.class,
+            ApplicationEnvironmentPreparedEvent.class, ApplicationPreparedEvent.class,
+            ContextClosedEvent.class, ApplicationFailedEvent.class };
+
+    private static Class<?>[] SOURCE_TYPES = { SpringApplication.class,
+            ApplicationContext.class };
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ApplicationEnvironmentPreparedEvent) {
+
+            ConfigurableEnvironment envi = ((ApplicationEnvironmentPreparedEvent) event).getEnvironment();
+
+            PropertySource<?> propertySource = envi.getPropertySources().get("applicationConfig: [classpath:/application.yml]");
+            //你自己的逻辑，切记MDC.put(key,value)
+            MDC.put("LOG_PATH", "./log/server");
+        }
+    }
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.springframework.core.Ordered#getOrder()
+     */
+    @Override
+    public int getOrder() {
+        // TODO Auto-generated method stub
+        return DEFAULT_ORDER;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.springframework.context.event.GenericApplicationListener#
+     * supportsEventType(org.springframework.core.ResolvableType)
+     */
+    @Override
+    public boolean supportsEventType(ResolvableType resolvableType) {
+        return isAssignableFrom(resolvableType.getRawClass(), EVENT_TYPES);
+    }
+
+    @Override
+    public boolean supportsSourceType(Class<?> sourceType) {
+        return isAssignableFrom(sourceType, SOURCE_TYPES);
+    }
+
+    private boolean isAssignableFrom(Class<?> type, Class<?>... supportedTypes) {
+        if (type != null) {
+            for (Class<?> supportedType : supportedTypes) {
+                if (supportedType.isAssignableFrom(type)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
